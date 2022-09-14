9305a6dca056e2c9bd6c40bf042c86e040e35741
luke <lusongyuan@lukedeMBP.lan>
2022-09-13 21:46:38 -0700

    author and reviewer

--- seeds/index.js
+++ seeds/index.js
@@ -26,6 +26,7 @@ const seedDb = async() =>{
         const random1000 = Math.floor(Math.random()*1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author:'6321515a87cefae5d0bb6929',
             location: `${cities[random1000].city},${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
